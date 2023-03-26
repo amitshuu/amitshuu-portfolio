c09f09db82c4111ccaabe9a5847cb97111942be4
Amit <amitshuu@gmail.com>
2023-03-26 21:57:07 +0300

    css changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import AboutSection from './components/AboutSection';
 import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
 import ProjectsSection from './components/ProjectsSection';
+import ProjectSection2 from './components/ProjectSection2';
 import ServicesSection from './components/ServicesSection';
 import 'react-multi-carousel/lib/styles.css';
+import ProjectModal from './components/ProjectModal';
 const App = () => {
   return (
-    <main className=' bg-dark-bg'>
+    <main className='w-full h-full bg-dark-bg'>
       <Navbar />
       <HeroSection />
       <AboutSection />
