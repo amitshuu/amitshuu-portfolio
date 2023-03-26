94342855bcef630f00b4a49360ea4b76d678b6fd
Amit <amitshuu@gmail.com>
2023-03-26 21:57:34 +0300

    Added new paragraph for mobile

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import about from '../assets/about_me_image2.png';
 // import test from '../assets/dev-interface.png';
 // import test from '../assets/optimization.png';
+
 const AboutSection = () => {
   return (
-    <section className='flex justify-between items-center py-10 bg-primary-section mt-20 section-center w-full '>
+    <section className='flex items-center justify-between w-full py-10 mt-20 bg-primary-section section-center '>
       <div className='flex'>
         <img
-          className='hidden xl:block max-w-lg'
+          className='hidden max-w-lg xl:block'
           src={about}
           alt={'About img'}
         />
       </div>
       <div className='flex flex-col items-center'>
-        <p className='text-white text-4xl lg:text-6xl py-4 '>
+        <p className='py-4 text-4xl text-white lg:text-6xl '>
           About <span className='text-primary-blue'>Me</span>
         </p>
-        <p className='text-gray-300  md:text-lg xl:w-3/4 text-center  font-normal text-md mb-5'>
+        <p className='hidden mb-5 font-normal text-center text-gray-300 md:block md:text-lg xl:w-3/4 text-md'>
           Hi, my name is Amit, and I'm a 23-year-old full-stack web developer
           with expertise in both front-end and back-end development. I
           specialize in a range of technologies, including JavaScript,
@@ -28,8 +29,14 @@ const AboutSection = () => {
           designing a sleek front-end interface or working on complex back-end
           functionality, I'm always striving to deliver high-quality results.
         </p>
+        <p className='block font-normal text-center text-gray-300 md:hidden xl:w-3/4 text-md'>
+          I'm Amit, a 23-year-old full-stack web developer with expertise in
+          front-end and back-end development. I'm skilled in JavaScript, React,
+          Node.js, and MongoDB, among other technologies. I enjoy building
+          user-friendly web applications and collaborating with others to bring
+          ideas to life. When I'm not coding, I like to surf and hit the gym.
+        </p>
       </div>
-      {/* <img src={test} className=' w-80' /> */}
     </section>
   );
 };
