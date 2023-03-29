7a81a5757460361d1032fbc8df5e2ce518ce1b6b
Amit <amitshuu@gmail.com>
2023-03-29 20:26:46 +0300

    Service section improvements

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -4,23 +4,23 @@ import { IService } from '../constants/types';
 const ServicesSection = () => {
   return (
     <>
-      <section className='flex flex-col items-center justify-center px-10 py-20 bg-dark-bg'>
-        <p className='mb-16 text-5xl text-center text-white xl:text-6xl'>
+      <section className='flex flex-col items-center justify-center w-full px-10 py-24 bg-dark-bg'>
+        <p className='mb-16 text-5xl text-center text-white xl:text-7xl'>
           My <span className='text-primary-blue'>Services</span>
         </p>
-        <div className='flex flex-wrap w-full lg:flex-nowrap lg:w-full'>
-          {services_links.map((service: IService) => {
+        <div className='flex flex-wrap w-full gap-10 justify-evenly xl:flex-nowrap'>
+          {services_links.map((service) => {
             return (
-              <article className='block w-full px-3 py-10 mb-10 text-center transition-transform cursor-pointer bg-primary-section md:mx-4 rounded-2xl hover:transform hover:scale-105'>
+              <article className='container flex flex-col items-center max-w-sm px-4 py-6 transition-transform cursor-pointer hover:transform hover:scale-105 rounded-2xl bg-primary-section'>
                 <img
-                  className='w-16 h-16 m-auto mb-2'
+                  className='w-20 h-20 mb-2'
                   src={service.serviceIcon}
-                  alt='Service Icon'
+                  alt='service icon'
                 />
-                <p className='inline-block max-w-[250px] md:max-w-full mb-3 text-lg font-semibold text-white border-b-2 border-primary-blue lg:max-w-[225px] xl:max-w-full'>
+                <p className='mb-4 text-xl text-white border-b-2 border-primary-blue'>
                   {service.serviceName}
                 </p>
-                <p className='m-0 text-lg text-gray-300'>
+                <p className='max-w-xs text-lg text-center text-gray-300 '>
                   {service.serviceDescription}
                 </p>
               </article>
