913389a0e69b372cf5f1c624aeb766b7d6ecec7d
Amit <amitshuu@gmail.com>
2023-03-31 20:56:03 +0300

    Added custom useForm hook

--- src/hooks/customHooks.ts
+++ src/hooks/customHooks.ts
@@ -21,3 +21,26 @@ export const useMobile = (callback: VoidFunction) => {
     return () => window.removeEventListener('resize', handleResize);
   }, [isMobile, callback]);
 };
+
+export const useForm = (callback: () => void, initialState: any) => {
+  const [values, setValues] = useState(initialState);
+
+  const onChange = (e: React.ChangeEvent<any>): void => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e: React.ChangeEvent<any>): void => {
+    e.preventDefault();
+    callback();
+  };
+
+  return {
+    values,
+    onChange,
+    onSubmit,
+    setValues,
+  };
+};
